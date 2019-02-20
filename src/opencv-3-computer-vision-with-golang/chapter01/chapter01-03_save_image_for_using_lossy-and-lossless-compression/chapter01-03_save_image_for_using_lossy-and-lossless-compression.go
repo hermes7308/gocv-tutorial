0f61c8aea86ff81914ff4db6fe8c15893d9a7762
hyunkeun-park <hermes7308@gmail.com>
2019-02-20 11:08:12 +0900

    add save image for using lossy and lossless compression example

---
+++ src/opencv-3-computer-vision-with-golang/chapter01/chapter01-03_save_image_for_using_lossy-and-lossless-compression/chapter01-03_save_image_for_using_lossy-and-lossless-compression.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	"gocv.io/x/gocv"
+)
+
+func main() {
+	imageFilePath := "../../data/Lena.png"
+	img := gocv.IMRead(imageFilePath, gocv.IMReadAnyColor)
+	if &img == nil {
+		log.Panic("Can not read Image file : ", imageFilePath)
+		return
+	}
+
+	// PNG Lossless Compression
+	compressedImageFilePathForPNG := "./Lena_compressed.png"
+	gocv.IMWriteWithParams(compressedImageFilePathForPNG, img, []int{gocv.IMWritePngCompression, 0})
+
+	save_img := gocv.IMRead(compressedImageFilePathForPNG, gocv.IMReadAnyColor)
+	if &save_img == nil {
+		log.Panic("Can not read Image file : ", compressedImageFilePathForPNG)
+		return
+	}
+
+	// JPG Lossy Compression
+	compressedImageFilePathForJPG := "./Lena_compressed.jpg"
+	gocv.IMWriteWithParams(compressedImageFilePathForJPG, img, []int{gocv.IMWriteJpegQuality, 0})
+}
