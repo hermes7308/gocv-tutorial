7d8e9ecf88d65e35aaaf30bb2fc0598a9ecd97e7
hyunkeun-park <hermes7308@gmail.com>
2019-02-22 10:04:57 +0900

    add making_your_app_interactive_via_handling_user_input_from_mouse

---
+++ src/opencv-4-computer-vision-with-golang/chapter01/chapter01-08_making_your_app_interactive_via_handling_user_input_from_mouse/chapter01-08_making_your_app_interactive_via_handling_user_input_from_mouse.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"C"
+	"gocv.io/x/gocv"
+	"log"
+)
+
+func main() {
+	imageFilePath := "../../data/Lena.png"
+	mat := gocv.IMRead(imageFilePath, gocv.IMReadAnyColor)
+	if mat.Empty() {
+		log.Panic("Can not read Image file : ", imageFilePath)
+		return
+	}
+
+	matCopied := gocv.NewMat()
+	mat.CopyTo(&matCopied)
+
+	for {
+		// gocv has yet no mouse event handler
+		// so I use selectROI for capture image
+		rectangle := gocv.SelectROI("Original Image", mat)
+		regionMat := matCopied.Region(rectangle)
+
+		if !regionMat.Empty() {
+			log.Println(regionMat)
+			capture := gocv.NewWindow("Capture")
+			capture.IMShow(regionMat)
+		}
+	}
+}
